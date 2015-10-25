cf4ad016688a78933fce22b74df3f12c8a5efd4f
Roman Piták <roman@pitak.net>
2015-10-25 20:49:34 +0100

    mock classes added

---
+++ src/DataTypes/ApiSurveySubmissionMode.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package romanpitak/dotmailer-api-v2-client
+ * @author Roman Piták <roman@pitak.net>
+ * @copyright MIT
+ */
+
+namespace DotMailer\Api\DataTypes;
+
+final class ApiSurveySubmissionMode
+{
+}
